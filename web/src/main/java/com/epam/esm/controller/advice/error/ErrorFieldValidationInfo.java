74d78eeab6cc501e981368ae4f29f45082942a12
Yuliya Buyalskaya <buyalskaya.yv@gmail.com>
2021-01-20 17:59:19 +0300

    delete constructor

--- web/src/main/java/com/epam/esm/controller/advice/error/ErrorFieldValidationInfo.java
+++ web/src/main/java/com/epam/esm/controller/advice/error/ErrorFieldValidationInfo.java
@@ -3,14 +3,12 @@ package com.epam.esm.controller.advice.error;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.NotNull;
 
 @Data
-@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
