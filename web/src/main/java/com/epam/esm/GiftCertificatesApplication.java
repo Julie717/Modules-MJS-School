d9dd3f75b8db11b28189bf2ca7384d646e664d04
Yuliya Buyalskaya <buyalskaya.yv@gmail.com>
2021-03-25 20:18:53 +0300

    build war

--- web/src/main/java/com/epam/esm/GiftCertificatesApplication.java
+++ web/src/main/java/com/epam/esm/GiftCertificatesApplication.java
@@ -2,11 +2,12 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Locale;
 
 @SpringBootApplication
-public class GiftCertificatesApplication {
+public class GiftCertificatesApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(GiftCertificatesApplication.class, args);
